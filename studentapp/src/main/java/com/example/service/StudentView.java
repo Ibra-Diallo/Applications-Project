6a1ea323c7db7da808c2f845d699a9000d96172c
Ibrahima Diallo <diallo.soryi96@gmail.com>
2022-06-10 13:46:50 -0500

    School Management application

---
+++ studentapp/src/main/java/com/example/service/StudentView.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+public class StudentView {
+	
+	public void printStudentDetails(String studentName, String studentRollNo) {
+		System.out.println("Student:");
+		System.out.println("Name: "+ studentName);
+		System.out.println("Roll number: "+ studentRollNo);
+	}
+
+}
